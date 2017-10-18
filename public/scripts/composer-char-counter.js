c97e3b2db9d083876d5d74be4614df49e0c6f54d
Rileygowan <t.rileygowan@gmail.com>
2017-10-18 21:47:37 +0000

    Update dump. New functionality includes: AJAX requests, tweet constructor and renderer

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,23 @@
+$(document).on('ready', function(){
+  const MAXCHARS = 140;
+  $('.new-tweet textarea').on('keyup', function(event){
+    const length = $(this).val().length;
+    const counter = $(this).parent().find('span');
+    let color = 'black';
+    counter.text(MAXCHARS - length);
+    length > MAXCHARS ? color = 'red' : color = 'black';
+    counter.css('color', color);
+  });
+});
+
+/* STACKOVERFLOW
+
+$(document).ready(function(){
+
+});
+
+$(function(){
+
+})
+
+*/
